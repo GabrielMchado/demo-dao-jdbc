5766a91a13aa29b44748d7388763e4e4329d20e9
Gabriel Machado <bieloliveiramachado@gmail.com>
2023-07-16 14:31:38 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,12 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SelleDaoJDBC;
 
 public class DaoFactory {
 
 	public static SellerDao createSellerDao() {
-		return new SelleDaoJDBC();
+		return new SelleDaoJDBC(DB.getConnection());
 	}
-	
 	
 }
