65742edad2719b96c9456441d56594edf1329f14
Gabriel Machado <bieloliveiramachado@gmail.com>
2023-07-20 20:12:24 -0300

    Department findAll implemantation

--- src/application/ProgramDepartment.java
+++ src/application/ProgramDepartment.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -10,9 +12,13 @@ public class ProgramDepartment {
 		
 		DepartmentDao depdao = DaoFactory.creatDepartmentDao();
 		
-		System.out.println("===== TEST 1: Seller findById ======");
+		System.out.println("===== TEST 1: Department findById ======");
 		Department dep = depdao.findById(2);
 		System.out.println(dep);
+		
+		System.out.println("\n===== TEST 2: Department findAll ======");
+		List<Department> list = depdao.findAll();
+		list.forEach(System.out::println);
 		
 	}
 	
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import db.DB;
@@ -63,8 +64,32 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 
 	@Override
 	public List<Department> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			ps = conn.prepareStatement("SELECT * FROM coursejdbc.department");
+			rs = ps.executeQuery();
+			
+			List<Department> list = new ArrayList<>();
+			
+			while(rs.next()) {
+				Department dep = instatiateDepartment(rs);
+				list.add(dep);
+			}
+			return list;
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(ps);
+		}
+	}
+	
+	private Department instatiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("Id"));
+		dep.setName(rs.getString("Name"));
+		return dep;
 	}
 
 }
